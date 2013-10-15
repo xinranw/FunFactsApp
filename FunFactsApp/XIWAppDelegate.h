c73b160cdf32b493e48590ed056e88dcc68222d1
Xinran Wang <xw1992@gmail.com>
2013-10-15 19:56:34 -0400

    Initial Commit

---
+++ FunFactsApp/XIWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XIWAppDelegate.h
+//  FunFactsApp
+//
+//  Created by User on 10/15/13.
+//  Copyright (c) 2013 Xinran Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XIWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FunFactsAppTests/FunFactsAppTests.m
@@ -0,0 +1,34 @@
+//
+//  FunFactsAppTests.m
+//  FunFactsAppTests
+//
+//  Created by User on 10/15/13.
+//  Copyright (c) 2013 Xinran Wang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FunFactsAppTests : XCTestCase
+
+@end
+
+@implementation FunFactsAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
